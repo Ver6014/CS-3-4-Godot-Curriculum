c574e50d50c857bb28c17afb9b9083fc6a590c85
Ver6014 <v.schulhoff@gmail.com>
2025-10-22 15:30:03 -0700

    23

--- scripts/araymanager.gd
+++ scripts/araymanager.gd
@@ -1,9 +1,14 @@
 extends Node
 const PLAYER = preload("uid://cqvpxnx2pwqhk")
 var inventor = PLAYER.inventor
-var item
+var item 
+var itempos
+
 func check_inventor():
 	print(inventor)
 
 func add_item():
 	inventor.insert(0, item)
+
+func remove_item():
+	inventor.remove_at(itempos)
