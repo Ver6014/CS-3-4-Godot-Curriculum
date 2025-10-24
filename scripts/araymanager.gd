27875efc692aec509810d218ea65aabe3be34f5c
Ver6014 <v.schulhoff@gmail.com>
2025-10-24 15:16:01 -0700

    update inventory

--- scripts/araymanager.gd
+++ scripts/araymanager.gd
@@ -15,8 +15,5 @@ func remove_item():
 func scelect_item():
 	pass
 
-
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(_area: Area2D) -> void:
 	pass # Replace with function body.
